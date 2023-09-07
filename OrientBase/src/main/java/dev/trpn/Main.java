ff5ebb47e42ab4a22ca5da4e3695b9e26970a2aa
Nolan <officialstackemup@gmail.com>
2023-09-07 12:57:54 -0700

    mnnb

--- OrientBase/src/main/java/dev/trpn/Main.java
+++ OrientBase/src/main/java/dev/trpn/Main.java
@@ -1,13 +1,16 @@
 package dev.trpn;
 
-import java.util.ArrayList;
 import java.util.List;
-
+import static dev.trpn.Interp.interpret;
 public class Main {
 
 public static void main(String[] args) {
 
 
+    String orientlangCode = "program HelloWorld { function main() { console.print(TESTIBG@!@); } }";
+    List<Tokenize> tokens = Lexer.tokenize(orientlangCode);
+    Parser.parse(tokens);
+    interpret(tokens);
 
 }
 
